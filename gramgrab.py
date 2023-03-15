8cebb1884168622f111ea72b32289608857c0d57
Mark Schmidt <MarkSchmidty@users.noreply.github.com>
2023-03-15 01:27:17 -0400

    more utf8 attempts

--- gramgrab.py
+++ gramgrab.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import asyncio
+import codecs
 from telethon import TelegramClient
 from telethon.tl.functions.channels import GetFullChannelRequest
 from telethon.tl.types import MessageMediaDocument
@@ -29,11 +30,12 @@ concurrent_downloads = args.concurrent_downloads
 
 async def download_zip_file(client, message):
     file_name = message.media.document.attributes[0].file_name
-    file_name_utf8 = bytes(file_name, 'utf-8', 'surrogateescape').decode('unicode_escape')
+    file_name_utf8 = codecs.decode(file_name, 'unicode_escape')
     file_path = os.path.join(os.getcwd(), file_name_utf8)
     print(f'Downloading {file_name_utf8}...')
     await client.download_media(message, file_path)
     print(f'{file_name_utf8} downloaded.')
+
 
 async def download_zip_files(client, channel_url):
     channel = await client.get_entity(channel_url)
